56eaf1a75eb18b4d232af3ef4421d721cbd7bf3e
Allard Thibaut <thibaut.allard@viacesi.fr>
2018-11-10 16:49:46 +0100

    compute addition of two int

--- Projet Fondamentaux Scientifique/main.c
+++ Projet Fondamentaux Scientifique/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition (a, b)
+{
+    return a+b;
+}
 int main()
 {
-    printf("Hello world!\n");
+    printf("d\n", addition(1,4));
     return 0;
 }
