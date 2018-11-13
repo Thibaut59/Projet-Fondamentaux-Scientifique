70b2517e2fb93556eed72bf6f6a64d551406d4f6
SamiKr <44946069+SamiKr@users.noreply.github.com>
2018-11-13 18:10:19 +0100

    Partie 3
    Debut du code en C

---
+++ Projet Fondamentaux Scientifique/CODE SAMI/PART 3/menu.c
@@ -0,0 +1,17 @@
+#include "menu.h"
+
+//Creation de la fonction menu
+void menu ()
+{
+printf("=====================================\n");
+printf("=| 1. Chenillard                   |=\n");
+printf("=| 2. Une LED sur deux             |=\n");
+printf("=| 3. Allumage complet             |=\n");
+printf("=| 4. Une LED sur trois            |=\n");
+printf("=| 5. LED au choix                 |=\n");
+printf("=====================================\n");
+int choix;
+scanf("%d",&choix);
+printf("Vous avez choisi le %i",choix);
+
+}
