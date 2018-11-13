b78792e8d16cb0a04e1c09118d952828c30b279f
SamiKr <44946069+SamiKr@users.noreply.github.com>
2018-11-13 14:01:21 +0100

    CODE SAMI

---
+++ Projet Fondamentaux Scientifique/CODE SAMI/Code en Arduino complet/param.h
@@ -0,0 +1,238 @@
+void chenillard(){
+  digitalWrite(6, HIGH);
+  delay(40);
+ 
+  digitalWrite(7, HIGH);
+  delay(40);
+  
+  digitalWrite(8, HIGH);
+  delay(40);
+ 
+  digitalWrite(9, HIGH);
+  delay(40);
+  
+  digitalWrite(10, HIGH);
+    delay(40);
+  
+  digitalWrite(11, HIGH);
+   delay(40);
+   
+delay(40);
+ digitalWrite(2,HIGH);
+ delay(40);
+
+  digitalWrite(3, HIGH);
+  delay(40);
+  
+  digitalWrite(4, HIGH);
+  delay(40);
+ 
+  digitalWrite(5,HIGH);
+  delay(40);
+
+ delay(1000);
+
+   digitalWrite(6, LOW);
+  delay(40);
+ 
+  digitalWrite(7,LOW);
+  delay(40);
+  
+  digitalWrite(8, LOW);
+  delay(40);
+ 
+  digitalWrite(9, LOW);
+  delay(40);
+  
+  digitalWrite(10, LOW);
+    delay(40);
+  
+  digitalWrite(11, LOW);
+   delay(40);
+   
+delay(40);
+ digitalWrite(2,LOW);
+ delay(40);
+
+  digitalWrite(3, LOW);
+  delay(40);
+  
+  digitalWrite(4, LOW);
+  delay(40);
+ 
+  digitalWrite(5,LOW);
+  delay(40);
+}
+
+void unesurdeux(){
+delay(600);
+digitalWrite(2,HIGH);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
+digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+digitalWrite(7,LOW);
+digitalWrite(8,HIGH);
+digitalWrite(9,LOW);
+digitalWrite(10,HIGH);
+digitalWrite(11,LOW);
+delay(300);
+digitalWrite(2,LOW);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(7,LOW);
+digitalWrite(8,LOW);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+digitalWrite(11,LOW);
+delay(600);
+digitalWrite(2,LOW);
+digitalWrite(3,HIGH);
+digitalWrite(4,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+digitalWrite(7,HIGH);
+digitalWrite(8,LOW);
+digitalWrite(9,HIGH);
+digitalWrite(10,LOW);
+digitalWrite(11,HIGH);
+delay(600);
+
+digitalWrite(2,LOW);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(7,LOW);
+digitalWrite(8,LOW);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+digitalWrite(11,LOW);
+}
+
+void allumagecomplet()
+{
+  
+delay (400);
+  digitalWrite(6, HIGH);
+  
+ digitalWrite(7, HIGH);
+
+ digitalWrite(8, HIGH);
+  
+ digitalWrite(9, HIGH);
+
+digitalWrite(10, HIGH);
+
+   digitalWrite(11, HIGH);
+ 
+   digitalWrite(2,HIGH);
+ 
+digitalWrite(3, HIGH);
+  
+digitalWrite(4, HIGH);
+
+ digitalWrite(5,HIGH);
+
+ delay (400);
+  digitalWrite(6, LOW);
+  
+ digitalWrite(7, LOW);
+
+ digitalWrite(8, LOW);
+  
+ digitalWrite(9, LOW);
+
+digitalWrite(10, LOW);
+
+   digitalWrite(11, LOW);
+ 
+   digitalWrite(2,LOW);
+ 
+digitalWrite(3, LOW);
+  
+digitalWrite(4, LOW);
+
+ digitalWrite(5,LOW);
+
+
+  
+delay (400);
+  digitalWrite(6, HIGH);
+  
+ digitalWrite(7, HIGH);
+
+ digitalWrite(8, HIGH);
+  
+ digitalWrite(9, HIGH);
+
+digitalWrite(10, HIGH);
+
+   digitalWrite(11, HIGH);
+ 
+   digitalWrite(2,HIGH);
+ 
+digitalWrite(3, HIGH);
+  
+digitalWrite(4, HIGH);
+
+ digitalWrite(5,HIGH);
+
+}
+
+void unesurtrois()
+{
+delay(600);
+digitalWrite(2,HIGH);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+digitalWrite(7,LOW);
+digitalWrite(8,HIGH);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+digitalWrite(11,HIGH);
+delay(300);
+digitalWrite(2,LOW);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(7,LOW);
+digitalWrite(8,LOW);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+digitalWrite(11,LOW);
+delay(600);
+digitalWrite(2,LOW);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(7,HIGH);
+digitalWrite(8,LOW);
+digitalWrite(9,LOW);
+digitalWrite(10,HIGH);
+digitalWrite(11,HIGH);
+delay(600);
+
+digitalWrite(2,LOW);
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(7,LOW);
+digitalWrite(8,LOW);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+digitalWrite(11,LOW);
+}
+
+void auchoix ()
+{
+int LED= 3;
+digitalWrite(LED,HIGH);
+}
