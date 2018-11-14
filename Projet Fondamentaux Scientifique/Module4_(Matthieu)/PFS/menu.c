62371385c1206ce9396fffab0bb2f2774ceaed89
DEWAELE Matthieu <matthieu.dewaele@viacesi.fr>
2018-11-14 09:55:51 +0100

    Prog C (recherche)

--- Projet Fondamentaux Scientifique/Module4_(Matthieu)/PFS/menu.c
+++ Projet Fondamentaux Scientifique/Module4_(Matthieu)/PFS/menu.c
@@ -7,13 +7,12 @@
 void afficherMenu(Valeurs *tabValeurs, int Taille)
 {
     int choix;
-    printf("taper 1 pour trier dans l'ordre croissant\n");
-    printf("taper 2 pour trier dans l'ordre croissant\n");
-    printf("taper 3 pour trier dans l'ordre croissant\n");
-    printf("taper 4 pour trier dans l'ordre croissant\n");
-    printf("taper 5 pour trier dans l'ordre croissant\n");
-    printf("taper 6 pour trier dans l'ordre croissant\n");
-    printf("taper 7 pour trier dans l'ordre croissant\n");
+    printf("|---taper 1 pour trier les valeurs--------------------|\n");
+    printf("|---taper 2 pour rechercher une valeurs---------------|\n");
+    printf("|---taper 3 pour afficher la moyenne des valeurs------|\n");
+    printf("|---taper 4 pour afficher le nombre de valeurs--------|\n");
+    printf("|---taper 5 pour afficher le minimum est le maximum---|\n");
+    printf("|---taper 6 pour afficher les valeurs-----------------|\n");
 
     scanf("%i", &choix);
 
@@ -23,16 +22,16 @@ void afficherMenu(Valeurs *tabValeurs, int Taille)
         tri(tabValeurs, Taille);
         break;
     case 2:
+    	rechercheDonnee(tabValeurs, Taille);
         break;
     case 3:
+//        afficherMoyenne(tabValeurs, Taille);
         break;
     case 4:
         break;
     case 5:
         break;
     case 6:
-        break;
-    case 7:
         break;
     default:
         break;
