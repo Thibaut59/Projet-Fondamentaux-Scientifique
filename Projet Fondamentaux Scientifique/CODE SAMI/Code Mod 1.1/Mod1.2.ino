8d74054902c0a895132d4b285da128a9fecfc72b
SamiKr <44946069+SamiKr@users.noreply.github.com>
2018-11-16 09:23:43 +0100

    Code Module 1.1
    Ne detecte pas le Serial print

---
+++ Projet Fondamentaux Scientifique/CODE SAMI/Code Mod 1.1/Mod1.2.ino
@@ -0,0 +1,12 @@
+#include "Cardio.h"
+int battement;
+int battement_moyen;
+int analogPin=0;
+bool b;
+float bpm_10_sec;
+float bpm_moyen;
+int seconde=10; //Initialisation des variables globales
+float tempsPasse = 0;
+float timeTemps = 0;
+
+loop() ;
