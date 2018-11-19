390abea1ee8814f152c2b2c493c9b3362851558d
Simon-Delayen <44962932+Simon-Delayen@users.noreply.github.com>
2018-11-19 10:36:12 +0100

    Update Poul_toute_les_10_secondes.ino

--- Projet Fondamentaux Scientifique/Module 3/Convertisseur_cinqui_me_test/Poul_toute_les_10_secondes/Poul_toute_les_10_secondes.ino
+++ Projet Fondamentaux Scientifique/Module 3/Convertisseur_cinqui_me_test/Poul_toute_les_10_secondes/Poul_toute_les_10_secondes.ino
@@ -1,12 +1,8 @@
 int analogPin=0;
-bool b;
+bool passage;
 
 float battement;
-float battement_moyen;
 float bpm_10_sec;
-float bpm_moyen;
-
-int seconde=10; //Initialisation des variables globales
 
 float tempsPasse = 0;
 
@@ -28,12 +24,11 @@ float tension=valeur * (5.0/1023.0); //opération pour changer l'échelle (avoir
  if (tension>=1.5)  //Si la pin A0 récupére une tension supérieur à 1.5 V alors...
  {
 
-  if (b == false) //b sert à ce que battement et battement_moyen ne s'incrémente qu'une fois quand tension >= 1.5
+  if (passage == false) //b sert à ce que battement et battement_moyen ne s'incrémente qu'une fois quand tension >= 1.5
   {
     
     battement++;  //battement toutes les 10 secondes
-    battement_moyen++;  //battement moyen pour tout le temps
-    b=true;
+    passage=true;
 
     
     delay(20);
@@ -42,7 +37,7 @@ float tension=valeur * (5.0/1023.0); //opération pour changer l'échelle (avoir
  }
  else
  {
-  b=false;  //les battements peuvent de nuveau s'incrémenter
+  passage=false;  //les battements peuvent de nuveau s'incrémenter
   delay(20);
  }
 
@@ -57,12 +52,8 @@ if(millis() >= (timeTemps +10000))  //dés que 10 secondes se passent...
   bpm_10_sec=(battement/10)*60; //on calcul les BPM pour les 10 derniéres secondes
   Serial.println(bpm_10_sec);
   battement=0;  //on réinitialise battement pour reprendre les nouveaux battements des 10 prochaines secondes
-
-  //Serial.print("BPM moyen: ");
-  //bpm_moyen=(battement_moyen/seconde)*60; //on calcul les BPM moyens pour tout le programme, on ne la réinitialise donc pas
-  //Serial.println(bpm_moyen);
   
-  seconde+=10;  //variables qui permet de calculer les BPM moyens toutes les 10 secondes
+  
   
 }
 
